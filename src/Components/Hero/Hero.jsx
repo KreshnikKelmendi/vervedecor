6cca8fee171a04942d2c99be9fe602708d7b620c
KreshnikKelmendi <kreshnik.kelmendi1994@gmail.com>
2023-10-19 16:47:34 -1200

    thirdlaunches

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import image1 from "../Assets/party.png"
 import image2 from "../Assets/party3.png"
 import image3 from "../Assets/wedding3.png"
@@ -16,33 +16,36 @@ function Hero() {
     setCurrentIndex((prevIndex) => (prevIndex - 1 + items.length) % items.length);
   };
 
-  const nextItem = () => {
+  const nextItem = useCallback(() => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % items.length);
-  };
+  }, [items.length]);
 
   const currentItem = items[currentIndex];
 
-  const autoplayInterval = 3000; 
+  const autoplayInterval = 3000;
 
   useEffect(() => {
     const autoplayTimer = setInterval(() => {
-      nextItem(); 
+      nextItem();
     }, autoplayInterval);
 
     return () => {
       clearInterval(autoplayTimer);
     };
-  }, [currentIndex]);
+  }, [currentIndex, nextItem]);
+
+
 
   return (
       <div className="slider relative bg-pink-700 grid grid-cols-1 md:grid-cols-2">
         <div className="slider-content col-span-1 md:col-span-1">
          <p className='text-3xl lg:w-[80vh] text-gray-300 text-center uppercase font-custom1  absolute top-[70vh] lg:top-1/2 lg:left-1/4 lg:transform lg:-translate-x-1/4 lg:-translate-y-1/2 p-4'>{currentItem.text}</p>
+       
         </div>
         <div className="slider-image col-span-1 md:col-span-1">
-          <img className='w-full h-[100vh] object-contain' src={currentItem.image} alt={`Image ${currentIndex + 1}`} />
+          <img className='w-full h-[100vh] object-contain' src={currentItem.image} alt=""/>
         </div>
-        <div className="arrow-button arrow-left absolute left-4 top-1/2 transform -translate-y-1/2 lg:bg-pink-700 rounded-full p-2 cursor-pointer hidden lg:block" onClick={prevItem}>
+        <div className="arrow-button arrow-left absolute left-4 top-1/2 transform -translate-y-1/2 lg:bg-pink-800 rounded-full p-2 cursor-pointer hidden lg:block" onClick={prevItem}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="32"
@@ -54,7 +57,7 @@ function Hero() {
             />
           </svg>
         </div>
-        <div className="arrow-button arrow-right absolute right-4 top-1/2 transform -translate-y-1/2 lg:bg-pink-700 rounded-full p-2 cursor-pointer hidden lg:block" onClick={nextItem}>
+        <div className="arrow-button arrow-right absolute right-4 top-1/2 transform -translate-y-1/2 lg:bg-pink-800 rounded-full p-2 cursor-pointer hidden lg:block" onClick={nextItem}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="32"
